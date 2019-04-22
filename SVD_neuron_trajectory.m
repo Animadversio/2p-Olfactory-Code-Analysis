077517c3dee833fcf0b679578ef7ed94262e3b87
kalfton <zkning1994@gmail.com>
2019-04-21 23:27:13 -0500

    Updated

--- SVD_neuron_trajectory.m
+++ SVD_neuron_trajectory.m
@@ -1,4 +1,5 @@
 close all; clear all;
+rng(0);
 spec_table = readtable('bnc_GH146_e51_2_LH.csv');
 %xlfiledata = xlsread('bnc_GH146_e51_2_LH.csv');
 load('raw_resp_GH146_e51_2.mat');
@@ -37,8 +38,20 @@ trial1 = [startpoint(1),endpoint(1)];
 alltrial = [startpoint(1),endpoint(end)];
 %odor2 = [1625,1740]-trial_start;
 
-Matrix1 = Big_Matrix(:,alltrial(1):alltrial(2));
+
+sorted_stim_name = {'PO', 'MH04', 'MH02', 'EB04', 'EB02', 'EA04', 'EA02', ...
+    'Bzald04', 'Bzald02', 'Acet04', 'Acet02', '1o3o04', '1o3o02'};
+sorted_timeid_list = [];
+for i = 1:length(sorted_stim_name)
+    row_id = find(contains(spec_table.stim1,sorted_stim_name{i}));
+    sorted_timeid_list = [sorted_timeid_list, (spec_table.trl_startStk(row_id):spec_table.trl_endStk(row_id))-1508];
+end
+
+Big_Matrix = Big_Matrix(:,sorted_timeid_list);
+
+%Matrix1 = Big_Matrix(:,alltrial(1):alltrial(2));
 % subtract baseline
+Matrix1 = Big_Matrix;
 baseline = mean(Big_Matrix(:,preodor),2);
 baseline = repmat(baseline,1,size(Matrix1,2));
 Matrix1 = Matrix1 - baseline;
@@ -48,6 +61,7 @@ figure;
 imagesc(Matrix1);hold on;
 for i = 1:length(startpoint)
     plot([startpoint(i),startpoint(i)], get(gca, 'Ylim'),'k');
+    text((stimstart(i) + startpoint(i))/2, -20, sorted_stim_name{i}, 'FontSize', 16)
 end
 %Big_Matrix = cellfun(@(x) eval(x(:)), A);
 [U,S,V] = svd(Matrix1);
@@ -57,7 +71,6 @@ for i = 1:4
     plotvector = i;
     plot(S(plotvector,plotvector)*V(:,plotvector));
 end
-
 
 % neuron trajectory
 
@@ -70,7 +83,7 @@ for i = 1:length(startpoint)
     trialtraj{i} = trajectory(:,startpoint(i):endpoint(i));
 end
 figure;
-plotstimuli = [11,13];
+plotstimuli = [4,5];
 for i = plotstimuli%length(startpoint)
     temp_plot = trialtraj{i};
 %     if i == 13
@@ -78,11 +91,17 @@ for i = plotstimuli%length(startpoint)
 %     end
     plot3(temp_plot(1,:),temp_plot(2,:),temp_plot(3,:)); hold on;
 end
-legend(stimname(plotstimuli));
+legend(sorted_stim_name(plotstimuli));
 
 % PCA and clustering (K-mean)
-Num_clusters = 13;
-[coeff,score,latent] = pca(Matrix1);
+% substract the control trial
+Trial_timepoints = 116;
+Trialnum = 5;
+Matrix2 = Matrix1 - repmat(Matrix1(:,1:Trial_timepoints),1,13);
+Matrix2 = Matrix2(:,(1:Trial_timepoints)+(Trialnum-1)*Trial_timepoints);
+
+Num_clusters = 4;
+[coeff,score,latent] = pca(Matrix2);
 pcascore = score(:,1:3);
 clusterID = kmeans(score(:,1:10),Num_clusters);
 figure;
@@ -94,18 +113,33 @@ zlabel('PC3');
 
 % evaluate cluster
 subplot(1,2,2);
-silhouette(Matrix1,clusterID);
+silhouette(Matrix2,clusterID);
 
 % sort by clusterID
 [~,I] = sort(clusterID);
 clusterID = clusterID(I,:);
-Sorted_Matrix = Matrix1(I,:);
+Sorted_Matrix = Matrix2(I,:);
 Sorted_neurondepth = neuron_depth(I,:);
 figure;
 imagesc(Sorted_Matrix); hold on;
 for i = 1:length(startpoint)
     plot([startpoint(i),startpoint(i)], get(gca, 'Ylim'),'k');
 end
+
+% plot the average response of each cluster
+figure;
+for i = 1:Num_clusters
+    subplot(4,4,i);
+    temp = Sorted_Matrix(clusterID == i,:);
+    plot(mean(temp));hold on;
+    title(['Cluster ID = ', mat2str(i)]);
+    ylim([-10,10]);
+%     xlim([1,1508]);
+%     for jj = 1:length(startpoint)
+%     plot([startpoint(jj),startpoint(jj)], get(gca, 'Ylim'),'k');
+%     end
+end
+
 
 % plot each cluster by the depth
 figure;
